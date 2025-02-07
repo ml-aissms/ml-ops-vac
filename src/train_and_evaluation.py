61b3c5fb3063cbce04038afd1033f4e6450820c3
Ashutosh <Ashutosh@LAPTOP-L53Q8KCU.local>
2025-02-07 09:52:57 +0530

    Evaluation file added

--- src/train_and_evaluation.py
+++ src/train_and_evaluation.py
@@ -1,14 +1,24 @@
 import os
 import argparse
 import pandas as pd
+import numpy as np
 from pkgutil import get_data
 from get_data import get_data, read_param
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 import json
 import mlflow #type:ignore
 from urllib.parse import urlparse
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
 
 def train_and_eval(config_path):
     config = read_param(config_path)
@@ -24,8 +34,43 @@ def train_and_eval(config_path):
     l1_ratio = config["estimator"]["ElasticNet"]["params"]["l1_ratio"]
 
     target = config["base"]["target_col"]
-    train_data = pd.read_csv("train_data_path")
-    test_data = pd.read_csv("test_data_path")
+    train_data = pd.read_csv(train_data_path)
+    test_data = pd.read_csv(test_data_path)
+
+    train_y = train_data[target]
+    test_y = test_data[target]
+
+    train_x = train_data.drop(target, axis=1)
+    test_x = test_data.drop(target, axis=1)
+
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
+    lr.fit(train_x,train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+
+    score_files = config["reports"]["scores"]
+    params_file = config["reports"]["params"]
+
+    with open(score_files, "w") as f:
+        scores = {
+            "rmse":rmse,
+            "mae":mae,
+            "re":r2
+        }
+        json.dump(scores,f)
+    
+    with open(params_file, "w") as f:
+        params = {
+            "alpha":alpha,
+            "l1_ratio":l1_ratio
+        }
+        json.dump(params,f)
+    
+    joblib.dump(lr,model_dir)
 
 
 
